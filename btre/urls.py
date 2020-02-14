e3f0d8f149fbababab5ad682ddee2fa5c8fcc5db
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-14 01:07:01 -0600

    se anadieron los 6 campos al listing y estilo al admin 1

--- btre/urls.py
+++ btre/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path 
 from django.urls import include #if you want to use 'include' keyword
 
+#new added
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     #
     path('', include('pages.urls')), #linking the urls.py of the pages app. #'' = homepage
     path('listings/', include('listings.urls')),
     #
-    path('admin/', admin.site.urls),   
-]
+    path('admin/', admin.site.urls),
+    
+    #new added
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
