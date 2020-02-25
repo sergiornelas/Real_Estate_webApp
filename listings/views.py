78412d37bfe8f57d0e2f48600711c39d8cbfa7c5
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-25 00:02:38 -0600

    datos a frontend parte 1

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+#pull data from listings model
+from .models import Listing
+
 def index(request):
-    return render(request, 'listings/listings.html')
+    #pull data from listings model
+    listings = Listing.objects.all()
+
+    #pull data from listings model
+    context = {
+        'listings': listings
+    }
+                                                #pull data from listings model
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
-
+ 
 def search(request):
     return render(request, 'listings/search.html')
