ca1b708a14533422aacbf2ce7a75b91b310b1894
sergiornelas <sergio.ornelas.92@outlook.com>
2020-03-04 00:36:55 -0600

    login listo

--- accounts/views.py
+++ accounts/views.py
@@ -49,10 +49,21 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 
+#user login
 def login(request):
     if request.method == 'POST':
-        #Login User
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+        #new method
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
